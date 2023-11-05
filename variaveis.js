c601ead0cb3196e5bda21c9f24c23e1c23575cf3
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-05 16:51:12 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,29 @@
+
+
+let idade = 26;
+
+
+console.log (idade);
+
+idade = 30;
+let numero = 30;
+let altura = 1.77;
+let nome = 'luis';
+let estutando = true;
+
+console.log (numero, typeof numero)
+console.log (altura, typeof altura)
+console.log (idade, typeof idade)
+console.log (estutando, typeof estutando)
+console.log (nome, typeof nome )
+
+
+let nula = null;
+
+console.log (nula)
+
+let variavel = 30;
+
+console.log (variavel);
+
+const constante = 100;
