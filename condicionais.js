5031d6e97cad59e83788881830df8f7088f8c7d8
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 09:17:54 -0300

    condicionais

---
+++ condicionais.js
@@ -0,0 +1,26 @@
+
+
+const idade = 10;
+
+if (idade >= 18) {
+    console.log('Voce e maior de idade')
+} 
+
+else {
+    console.log('Voce e menor  de idade')
+}
+
+console.clear()
+
+let media = 6
+
+if (media >=7) {
+    console.log('Aprovado(a)')
+} else if (media >=5){
+    console.log('Recuperação')
+}
+else {
+    console.log('Reprovado')
+}
+    
+
