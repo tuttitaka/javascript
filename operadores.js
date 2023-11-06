711eccc45d6a5e53a0629cf6c218b633a17481f8
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 08:19:05 -0300

    operadores

---
+++ operadores.js
@@ -0,0 +1,19 @@
+let numero1 = 20;
+let numero2 = 3;
+
+
+console.log('Soma =', numero1 + numero2)
+console.log('Subtração =', numero1 - numero2)
+console.log('Mutiplicação =', numero1 * numero2)
+console.log('Divisão =', numero1 / numero2)
+console.log('Exponenciação =', numero1 ** numero2)
+console.log('Resto da divisão=', numero1 % numero2)
+
+
+console.log((3 + 4) *2)
+console.log((10 +8 + 9) / 3)
+
+
+
+console.log(Math.PI)
+console.log(Math.sqrt(16))
