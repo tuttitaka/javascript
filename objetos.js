92852c431f63aef9775dc581c6e988413d3d0593
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 11:08:52 -0300

    objetos

---
+++ objetos.js
@@ -0,0 +1,17 @@
+
+
+
+let pessoa = {
+    nome: 'luis',
+    idade: 21,
+}
+console.log(pessoa)
+
+
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+
+
+pessoa.altura = 1.89
+
+console.log(pessoa)
