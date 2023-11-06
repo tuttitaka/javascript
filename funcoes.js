fe762db964c60c844a800cf610d9a41fde9f5014
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 10:58:59 -0300

    funções

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+ function saudacao() {
+    console.log('Ola,seja bem vindo(a) ao meu repositorio no github')
+ }
+
+
+ saudacao ()
+console.clear()
+
+
+function saudacao(nome) {
+    console.log('Ola',(nome),'seja bem vindo(a) ao meu repositorio no github')
+ }
+
+
+ saudacao ('luis')
+console.clear()
+
+ function soma(numero1, numero2){
+    return numero1 + numero2
+
+ }
+
+ const resultado = soma (10, 20 )
+
+ console.log(resultado)
