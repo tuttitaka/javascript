9127bcdc8495362f9c7837711c04ee6646e2f907
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 09:02:22 -0300

    coerção

---
+++ coercao.js
@@ -0,0 +1,17 @@
+
+
+const numero = 10;
+
+console.log (numero, typeof numero);
+
+
+const numeroF = String(numero)
+console.log(numeroF, typeof numeroF);
+
+console.log(Number('123123'))
+console.log(parseFloat('132132.123'))
+console.log(Boolean(1));
+
+console.clear
+
+console.log(10 + '1')
